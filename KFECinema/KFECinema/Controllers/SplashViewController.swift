8b70f590c58e1b562f40468dea45aca48ac296fd
vinod2324 <50589090+vinod2324@users.noreply.github.com>
2020-12-05 22:14:13 +0530

    Add initial projects

---
+++ KFECinema/KFECinema/Controllers/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashViewController.swift
+//  KFECinema
+//
+//  Created by Veerachamy, Vimal on 12/2/20.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+}
